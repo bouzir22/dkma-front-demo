2873fbf7f388cf4ede34d6ecf1cf4dcbad6ea5f5
unknown <ahmedbouzir1995@gmail.com>
2021-10-27 16:17:03 +0100

    demo commit

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require ('express');
-const path = require ('path');
-
+const express = require('express');
 const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
+const server = require('http').Server(app);
+
+app.use(express.static(__dirname, 'dist', {index: false}));
 
 
-app.use (express.static(__dirname + '/dist/DKMAF'));
+server.listen(port, function() {
+    console.log("App running on port " + port);
+})
 
-app.get('/*',function(req,res) { res.sendFile(path.join(__dirname + '/dist/DKMAF/index.html')) ;}
+// PathLocationStrategy
 
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
 
-);
-app.listen(process.env.PORT || 3000);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
