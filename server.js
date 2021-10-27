7abe1a5b36524b5d5a1febfd994d175edb8bede0
unknown <ahmedbouzir1995@gmail.com>
2021-10-27 15:50:02 +0100

    third commit

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require ('path');
 const app = express();
 
 
-app.use (express.static(__dirname+'/dist/DKMAF'));
+app.use (express.static(__dirname +'/dist/DKMAF'));
 
-app.get('/*',(req,res)=> res.sendFile(path.join(__dirname+'/dist/DKMAF/index.html')),
+app.get('/*',(req,res)=> res.sendFile(path.join(__dirname + '/dist/DKMAF/index.html')),
 
 
 );
