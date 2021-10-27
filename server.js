309ddd7e4d64ab5a252ed27ccc396087b8283f09
unknown <ahmedbouzir1995@gmail.com>
2021-10-27 16:05:11 +0100

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use (express.static(__dirname + '/dist/DKMAF'));
 
-app.get('/*',(req,res)=> res.sendFile(path.join(__dirname + '/dist/DKMAF/index.html')),
+app.get('/*',function(req,res) { res.sendFile(path.join(__dirname + '/dist/DKMAF/index.html')) ;}
 
 
 );
